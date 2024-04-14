04c2a369bde56756d538443865adb8541417d269
pimenteljoao44 <joaovitordasilvapimentel0@gmail.com>
2024-04-14 15:04:26 -0300

    first commit

---
+++ src/main/java/com/example/demo/controle/EmailController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controle;
+
+import com.example.demo.dtos.EmailDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class EmailController {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestBody EmailDTO emailDTO) {
+        try {
+            SimpleMailMessage mensagem = new SimpleMailMessage();
+            mensagem.setTo("joaovitordasilvapimentel0@gmail.com");
+            mensagem.setSubject("Novo Email de " + emailDTO.getNome());
+            mensagem.setText(emailDTO.getMensagem());
+
+            javaMailSender.send(mensagem);
+
+            return "Email enviado com sucesso!";
+        } catch (MailException e) {
+            return "Erro ao enviar o email: " + e.getMessage();
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/dtos/EmailDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.dtos;
+
+public class EmailDTO {
+
+    private String nome;
+    private String email;
+    private String mensagem;
+
+    public EmailDTO() {
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
